1494e615555bd5b4fe1e77829716bfa46e014cdd
Prajwol Gautam <prazzolgautam@gmail.com>
2024-11-13 22:44:14 +0545

    format login response output

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,13 +19,13 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ where: { username } });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ status: false, message: 'Invalid credentials' });
     }
 
     const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
-    res.status(200).json({ token });
+    res.status(200).json({ status: true, message: 'Login Sucessful !',data: {token: token,user: user} });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
