e3c78553646a9b9edeaedfb85dc94aa8742ebddb
Prajwol Gautam <prazzolgautam@gmail.com>
2024-11-18 22:12:59 +0545

    multiple changes db config changes .env.example creation

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const config = {
+  "development": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.DATABASE_HOST,
+    "dialect": process.env.DATABASE_CONNECTION
+  },
+  "test": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.DATABASE_HOST,
+    "dialect": process.env.DATABASE_CONNECTION
+  },
+  "production": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.DATABASE_HOST,
+    "dialect": process.env.DATABASE_CONNECTION
+  }
+}
+
+module.exports = config;
