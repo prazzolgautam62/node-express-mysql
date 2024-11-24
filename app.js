e802976660ebb0cead2d6e65f506e99d17ba563d
Prajwol Gautam <prazzolgautam@gmail.com>
2024-11-24 22:11:36 +0545

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
